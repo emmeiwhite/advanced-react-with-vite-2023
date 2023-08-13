73a015ae9120ba5726b1488155e4e804e15bd242
Imran Rafiq Rather <emmeiwhite@gmail.com>
2023-08-14 00:50:48 +0530

    Mapped over the list of person array

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,45 @@
+import { data } from "../../../data";
+import { useState } from "react";
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+
+  const deleteItem = (id) => {
+    setPeople((prevPeople) => {
+      const updatedPeople = prevPeople.filter((person) => person.id !== id);
+      return updatedPeople;
+    });
+  };
+
+  const deleteAll = () => {
+    setPeople([]);
+  };
+  return (
+    <>
+      <ul>
+        {people.map((person) => {
+          return (
+            <li key={person.id}>
+              <h4>{person.name}</h4>
+              <button
+                onClick={() => deleteItem(person.id)}
+                className="btn"
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+
+        <p>--------------------------------------------</p>
+        <button
+          onClick={deleteAll}
+          className="btn"
+        >
+          Delete All
+        </button>
+      </ul>
+    </>
+  );
 };
 
 export default UseStateArray;
