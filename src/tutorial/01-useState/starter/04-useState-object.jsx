a86181792d29ed402d6f6c44a92999010d3de0f3
Imran Rafiq Rather <emmeiwhite@gmail.com>
2023-08-14 01:34:52 +0530

    useState(object)

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,35 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    id: 1,
+    name: "Immu",
+    age: 31,
+    hobby: "Reading Books",
+  });
+
+  const handleClick = () => {
+    setPerson({
+      id: 2,
+      name: "Tim David",
+      age: 32,
+      hobby: "Cricket",
+    });
+  };
+  return (
+    <div>
+      <h4>Name: {person.name}</h4>
+      <h4>Age: {person.age}</h4>
+      <h4>Hobby: {person.hobby}</h4>
+
+      <button
+        onClick={handleClick}
+        className="btn"
+      >
+        Update Person
+      </button>
+    </div>
+  );
 };
 
 export default UseStateObject;
