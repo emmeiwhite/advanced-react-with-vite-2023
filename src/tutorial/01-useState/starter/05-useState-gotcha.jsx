f67dbb30917faf254f8170ff7ae2803248defc6f
Imran Rafiq Rather <emmeiwhite@gmail.com>
2023-08-14 02:35:05 +0530

    Passing function as argument to setState and updating state in more controlled way

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [count, setCount] = useState(0);
+
+  const handleCount = () => {
+    /*
+    setCount(count + 1);
+    console.log(count); // setCount is Asynchronous in nature
+    */
+
+    setCount((prevCount) => {
+      const newState = prevCount + 1;
+      console.log(newState);
+      return newState;
+    });
+  };
+  return (
+    <div>
+      <h2>{count}</h2>
+      <button
+        className="btn"
+        onClick={handleCount}
+      >
+        increase
+      </button>
+    </div>
+  );
 };
 
 export default UseStateGotcha;
