c17d75b8c5d6f1b988506149bb3ff49eef8bca24
Imran Rafiq Rather <emmeiwhite@gmail.com>
2023-08-20 20:57:48 +0530

    terniary challenge done

--- src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,46 @@
+import { useState } from "react";
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [user, setUser] = useState(null);
+
+  function login() {
+    // normally connect to db or api
+    console.log("Login invoked!");
+    setUser({
+      name: "Correct User",
+    });
+  }
+
+  function logout() {
+    console.log("logout invoked!");
+    setUser(null);
+  }
+
+  return (
+    <section>
+      {user ? (
+        <div>
+          <h2>Hello dear User</h2>
+          <button
+            className="btn"
+            onClick={logout}
+          >
+            Logout
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h2>You need to login to proceed</h2>
+          <button
+            className="btn"
+            onClick={login}
+          >
+            Login
+          </button>
+        </div>
+      )}
+    </section>
+  );
 };
 
 export default UserChallenge;
