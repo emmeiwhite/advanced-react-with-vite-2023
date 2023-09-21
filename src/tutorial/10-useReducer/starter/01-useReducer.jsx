aca2b577f2482fbe86f94ad025589dbce22ee2f9
Imran Rafiq Rather <emmeiwhite@gmail.com>
2023-09-22 04:13:16 +0530

    useReducer at its best

--- src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,27 +1,55 @@
-import React from 'react';
-import { data } from '../../../data';
+import React, { useReducer } from "react";
+import { data } from "../../../data";
+
+// Default State
+const defaultState = {
+  people: data,
+};
+
+// Reducer
+const reducer = (state, action) => {
+  if (action.type === "CLEAR_LIST") {
+    return {
+      ...state,
+      people: [],
+    };
+  }
+
+  return state;
+};
+
 const ReducerBasics = () => {
-  const [people, setPeople] = React.useState(data);
+  // const [people, setPeople] = React.useState(data);
+  const [state, dispatch] = useReducer(reducer, defaultState);
 
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
+    let newPeople = state.people.filter((person) => person.id !== id);
     setPeople(newPeople);
   };
+
+  const handleReset = () => {
+    dispatch({ type: "CLEAR_LIST" });
+  };
+
   return (
     <div>
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person;
         return (
-          <div key={id} className='item'>
+          <div
+            key={id}
+            className="item"
+          >
             <h4>{name}</h4>
             <button onClick={() => removeItem(id)}>remove</button>
           </div>
         );
       })}
+
       <button
-        className='btn'
-        style={{ marginTop: '2rem' }}
-        onClick={() => setPeople([])}
+        className="btn"
+        style={{ marginTop: "2rem" }}
+        onClick={handleReset}
       >
         clear items
       </button>
