124c66b1d8975ebb5d8221aef6100ac1e25bacf0
Imran Rafiq Rather <emmeiwhite@gmail.com>
2023-11-20 13:11:15 +0530

    Final Revision of React with John Smilga"

--- src/tutorial/01-useState/starter/01-error-example.jsx
+++ src/tutorial/01-useState/starter/01-error-example.jsx
@@ -2,72 +2,9 @@ import { useState } from "react";
 import { data } from "../../../data";
 
 const ErrorExample = () => {
-  // Question is: Why useState() the first place
-
-  /*
-  let count = 0;
-  const updateDateCount = () => {
-    count += 1;
-    console.log(count);
-  };
-  */
-
-  // We need a useState hook to trigger a re-render and to preserve the state during these re-renders
-
-  const [count, setCount] = useState(0);
-  const [persons, setPersons] = useState(data);
-
-  const updateDateCount = () => {
-    setCount((prevState) => {
-      return prevState + 1;
-    });
-  };
-
-  // ClearAll
-  const clearAll = () => {
-    setPersons([]);
-  };
-
-  const handleDelete = (id) => {
-    setPersons((prevPersons) => {
-      const updatedPersons = prevPersons.filter((person) => person.id !== id);
-      return updatedPersons;
-    });
-  };
-
   return (
     <div>
-      <h1>{count}</h1>
-      <button
-        className="btn"
-        onClick={updateDateCount}
-      >
-        Update Count
-      </button>
-
-      {persons.map((person) => {
-        return (
-          <div
-            className="person"
-            key={person.id}
-          >
-            <p>{person.name}</p>
-            <button
-              className="btn"
-              onClick={() => handleDelete(person.id)}
-            >
-              Delete
-            </button>
-          </div>
-        );
-      })}
-
-      <button
-        className="btn"
-        onClick={clearAll}
-      >
-        Clear All
-      </button>
+      <h1>useState Error Example</h1>
     </div>
   );
 };
