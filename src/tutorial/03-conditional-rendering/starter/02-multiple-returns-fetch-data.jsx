3b25c6fb5ac8a226e7249d185aed5bf6811fd293
Imran Rafiq Rather <emmeiwhite@gmail.com>
2023-12-13 08:34:04 +0530

    Mulitple returns loading, error and API

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -5,20 +5,39 @@ const MultipleReturnsFetchData = () => {
   /** In this challenge, I want to get the github user from an API */
   const [isLoading, setIsLoading] = useState(true);
   const [person, setPerson] = useState(null);
+  const [error, setError] = useState(false);
 
   const fetchUser = async () => {
-    const response = await fetch(url);
-    const user = await response.json();
-    console.log(user);
+    try {
+      const response = await fetch(url);
+      const user = await response.json();
+      console.log(user);
+
+      setIsLoading(false);
+      setPerson(user);
+    } catch (e) {
+      setError(true);
+      setIsLoading(false);
+      console.log(error);
+    }
   };
 
   useEffect(() => {
     fetchUser();
   }, []);
 
+  /** Order matters here:
+   * First comes loading, then error and only then the normal JSX return from the component
+   */
+
   if (isLoading) {
     return <h1>Loading ...</h1>;
   }
+
+  if (error) {
+    return <h1>There was an error ...</h1>;
+  }
+
   return <article>I am a Component returning some JSX</article>;
 };
 export default MultipleReturnsFetchData;
