8832a8167362a49b0c05bd7d2a06806c1f34ee63
Imran Rafiq Rather <emmeiwhite@gmail.com>
2023-12-20 02:17:44 +0530

    useReducer actions and actions types

--- src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,28 +1,60 @@
 import React, { useReducer, useState } from "react";
 import { data } from "../../../data";
 
+const defaultState = {
+  users: data,
+  isLoading: false,
+};
+
+const CLEAR_LIST = "CLEAR_LIST";
+const RESET_LIST = "REST_LIST";
+const REMOVE_ITEM = "REMOVE_ITEM";
+
+const reducer = (state, action) => {
+  if (action.type === CLEAR_LIST) {
+    return {
+      ...state,
+      users: [],
+    };
+  }
+
+  if (action.type === RESET_LIST) {
+    return {
+      ...state,
+      users: data,
+    };
+  }
+  // return state;
+  throw new Error(`No matching "${action.type}" - ACTION TYPE`);
+};
+
 function ReducerBasics() {
-  const [users, setUsers] = useState(data);
+  // const [users, setUsers] = useState(data);
+
+  const [state, dispatch] = useReducer(reducer, defaultState);
+  /* --- reducer function: We pass a reducer function & the default state --- */
   // Delete individual | D of CRUD
   const handleRemove = (id) => {
-    setUsers(users.filter((user) => user.id !== id));
+    // setUsers(users.filter((user) => user.id !== id));
   };
 
   // Remove All
   const handleClearAll = () => {
-    setUsers([]);
+    // setUsers([]);
+    dispatch({ type: CLEAR_LIST });
   };
 
   // Reset
   const handleReset = () => {
-    setUsers(data);
+    dispatch({ type: RESET_LIST });
+    // setUsers(data);
   };
 
-  const renderUsers = users.map((user) => {
+  const renderUsers = state.users.map((user) => {
     const { id, name } = user;
     return (
-      <div>
-        <p>{name}</p>
+      <div key={id}>
+        <h2>{name}</h2>
         <button
           className="btn"
           onClick={() => handleRemove(id)}
@@ -35,24 +67,21 @@ function ReducerBasics() {
 
   return (
     <div>
-      {users.length === 0 ? (
-        <button
-          className="btn"
-          onClick={handleReset}
-        >
-          Reset
-        </button>
-      ) : (
-        renderUsers
-      )}
-
+      {renderUsers}
       <br />
-      {users.length > 0 && (
+      {state.users.length > 0 ? (
         <button
           className="btn"
           onClick={() => handleClearAll()}
         >
           Clear All
+        </button>
+      ) : (
+        <button
+          className="btn"
+          onClick={handleReset}
+        >
+          Reset
         </button>
       )}
     </div>
