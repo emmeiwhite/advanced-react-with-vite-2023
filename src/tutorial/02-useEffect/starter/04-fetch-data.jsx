a023bbf4e3dfc6c6d0a7e8cec51cdeb5f651012e
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-06-03 23:28:57 +0530

    3 states ---> data, error, loading

--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,23 +1,44 @@
-import React, { useEffect, useState } from "react";
-const url = "https://api.github.com/users";
+import React, { useEffect, useState } from 'react'
+
+const url = 'https://api.github.com/users'
 
 const FetchData = () => {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState('')
+
+  const fetchUsers = async () => {
+    try {
+      setIsLoading(true)
+      const response = await fetch(url)
+      console.log(response)
+      if (response.ok) {
+        const data = await response.json()
+        setUsers(data)
+        setIsLoading(false)
+      } else {
+        throw new Error('There was an error loading content')
+      }
+    } catch (error) {
+      console.log(error)
+      setIsLoading(false)
+      setError('Error!')
+    }
+  }
   // For side-effects we use useEffect() hook e.g, making an API call
-
   useEffect(() => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        setUsers(data);
-      });
+    fetchUsers()
+  }, [])
 
-    return () => {
-      // called when component unmounts and also on each state update when the state is provided as the dependency array!
-    };
-  }, []);
+  if (isLoading) {
+    return <h1>loading ...</h1>
+  }
 
-  const renderUsers = users.map((user) => (
+  if (error) {
+    return <h1>{error}</h1>
+  }
+
+  const renderUsers = users.map(user => (
     <div
       className="user"
       key={user.id}
@@ -28,12 +49,12 @@ const FetchData = () => {
       />
       <p>{user.login}</p>
     </div>
-  ));
+  ))
 
   return (
     <section>
       <div className="users">{users.length > 0 && renderUsers}</div>
     </section>
-  );
-};
-export default FetchData;
+  )
+}
+export default FetchData
